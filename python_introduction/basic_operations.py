d65f5339de33b1329aa55fb9a08e32f9782a66e6
Mevin Ngugi <16956310+mevinngugi@users.noreply.github.com>
2024-06-23 17:06:52 +0300

    Add basic operations.py that handles Addition, Subtraction and Multiplication.

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+# A Python script that performs basic arithmetic operations with two predefined numbers.
+number1 = 10
+number2 = 5
+
+# Addition
+print(f"Addition of {number1} and {number2} is {number1 + number2}")
+
+# Subtraction
+print(f"Subtraction of {number1} and {number2} is {number1 - number2}")
+
+# Multiplication
+print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
