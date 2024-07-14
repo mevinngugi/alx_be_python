ec2bd47d68700678f2f922844d7c7764ae2020af
Mevin Ngugi <16956310+mevinngugi@users.noreply.github.com>
2024-07-14 18:08:30 +0300

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -18,14 +18,15 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(1, 2), -1)
         self.assertEqual(self.calc.subtract(-2, -2), 0)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         """Test the multiply method."""
         self.assertEqual(self.calc.multiply(2, 2), 4)
         self.assertEqual(self.calc.multiply(3, -2), -6)
         self.assertEqual(self.calc.multiply(-4, -2), 8)
         self.assertEqual(self.calc.multiply(0, 9), 0)
 
-    def test_divide(self):
+    def test_division(self):
+        """Test the divide method."""
         self.assertEqual(self.calc.divide(4, 2), 2)
         self.assertEqual(self.calc.divide(6, -2), -3)
         #with self.assertRaises(ValueError):
