30f723ffacf56d9e3f22d896a85c4dd84904458f
Mevin Ngugi <16956310+mevinngugi@users.noreply.github.com>
2024-07-14 18:03:25 +0300

    Add simple_calculator.py and test_simple_calculator.py

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,34 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self) -> None:
+        """Set up the SimpleCalculator instance before every test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-2, -2), -4)
+
+    def test_subtract(self):
+        """Test the subtract method."""
+        self.assertEqual(self.calc.subtract(3, 2), 1)
+        self.assertEqual(self.calc.subtract(1, 2), -1)
+        self.assertEqual(self.calc.subtract(-2, -2), 0)
+
+    def test_multiply(self):
+        """Test the multiply method."""
+        self.assertEqual(self.calc.multiply(2, 2), 4)
+        self.assertEqual(self.calc.multiply(3, -2), -6)
+        self.assertEqual(self.calc.multiply(-4, -2), 8)
+        self.assertEqual(self.calc.multiply(0, 9), 0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(4, 2), 2)
+        self.assertEqual(self.calc.divide(6, -2), -3)
+        #with self.assertRaises(ValueError):
+        #    self.calc.divide(10, 0)
+        self.assertEqual(self.calc.divide(10, 0), None)
+        
