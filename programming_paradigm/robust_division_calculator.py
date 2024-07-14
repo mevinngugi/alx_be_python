b75338b7c9de9cd7a5f5be26c273ae92d1894d2e
Mevin Ngugi <16956310+mevinngugi@users.noreply.github.com>
2024-07-14 13:46:50 +0300

    Convert inputs to float

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,7 +2,7 @@ def safe_divide(numerator, denominator):
     """A division calculator that robustly handles errors like
     division by zero and non-numeric inputs"""
     try:
-        return f"The result of the division is {int(numerator) / int(denominator)}"
+        return f"The result of the division is {float(numerator) / float(denominator)}"
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     #except TypeError:
