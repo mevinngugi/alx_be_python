21676a8d92d7b729df283609b99526952f6be695
Mevin Ngugi <16956310+mevinngugi@users.noreply.github.com>
2024-07-14 18:06:28 +0300

    Update test_subtract to test_subtraction

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,7 +12,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(-2, -2), -4)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         """Test the subtract method."""
         self.assertEqual(self.calc.subtract(3, 2), 1)
         self.assertEqual(self.calc.subtract(1, 2), -1)
@@ -31,4 +31,3 @@ class TestSimpleCalculator(unittest.TestCase):
         #with self.assertRaises(ValueError):
         #    self.calc.divide(10, 0)
         self.assertEqual(self.calc.divide(10, 0), None)
-        
