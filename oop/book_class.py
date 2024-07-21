bb96a5f4c5433ccf0014a132c83453fd365fd2fe
Mevin Ngugi <mevinngugi@ONAF-KE-MNGUGI.local>
2024-07-21 23:11:54 +0300

    Add book_class.py

---
+++ oop/book_class.py
@@ -0,0 +1,19 @@
+class Book:
+    """
+        A Book class that incorporates constructors (__init__),
+        destructors (__del__), and the representation methods (__str__ and __repr__).
+    """
+
+    def __init__(self, title, author, year):
+        self.title = str(title)
+        self.author = str(author)
+        self.year = int(year)
+
+    def __del__(self):
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year}))"
